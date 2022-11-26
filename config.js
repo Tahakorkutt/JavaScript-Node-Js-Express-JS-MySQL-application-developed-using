eccc46e19c499f064d255395ccb550688bc715b7
Tahakorkutt <111304410+Tahakorkutt@users.noreply.github.com>
2022-11-27 01:20:00 +0300

    developed application file
    JavaScript-Node Js- Express Js- MySQL

---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "tahakorkut123",
+        database: "blogdb"
+    },
+    email: {
+        username: "<email>",
+        password: "<parola>",
+        from: "<from_email>",
+    }
+}
+
+module.exports = config;
